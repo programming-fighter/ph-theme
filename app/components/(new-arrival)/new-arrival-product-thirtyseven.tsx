961628ea3f5b5cb4e4e984fa74a8b06585273f1e
Your New Name <Your new email>
2024-04-15 11:57:48 +0600

    image location change

--- app/components/(new-arrival)/new-arrival-product-thirtyseven.tsx
+++ app/components/(new-arrival)/new-arrival-product-thirtyseven.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import img from "@/assets/bgImage/thirtySeven/MARGIN.png";
+import img from "@/components/(new-arrival)/bg-img/37/MARGIN.png";
 import Image from "next/image";
 import Card64 from "../(card)/card64";
 
@@ -9,14 +9,14 @@ const NewArrivalProductThirtySeven = ({ product, design, store_id }: any) => {
     return null;
   }
   return (
-    <div className='shadow-lg py-5 sm:py-10 rounded-sm bg-[#F1F9DD]'>
-      <div className='sm:container px-5'>
+    <div className="shadow-lg py-5 sm:py-10 rounded-sm bg-[#F1F9DD]">
+      <div className="sm:container px-5">
         <div>
-          <Image src={img} alt='margin' className='mx-auto' />
-          <h1 className='text-2xl text-center'>NEW ARRIVAL PRODUCTS</h1>
+          <Image src={img} alt="margin" className="mx-auto" />
+          <h1 className="text-2xl text-center">NEW ARRIVAL PRODUCTS</h1>
         </div>
-        <div className='flex justify-center mt-10'>
-          <div className='grid grid-cols-2 md:grid-cols-3 gap-1 sm:gap-3 lg:grid-cols-5 xl:grid-cols-6 justify-center'>
+        <div className="flex justify-center mt-10">
+          <div className="grid grid-cols-2 md:grid-cols-3 gap-1 sm:gap-3 lg:grid-cols-5 xl:grid-cols-6 justify-center">
             {product?.slice(0, 12).map((item: any, id: any) => (
               <Card64
                 design={design}
