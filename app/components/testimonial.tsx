6e23e6ede1c890f306a21bfc0b79c7bbf6ad3da6
ninjastorm24 <siamabdullah1100@gmail.com>
2024-04-18 17:37:56 +0600

    testimonial 2

--- app/components/testimonial.tsx
+++ app/components/testimonial.tsx
@@ -1,12 +1,16 @@
 import TestimonialFive from "@/components/(testimonial)/testimonial-five";
 import TestimonialThree from "./(testimonial)/default";
 import TestimonialOne from "./(testimonial)/testimonial-one";
+import TestimonialTwo from "./(testimonial)/testimonial-two";
 
 const Testimonial = ({ testimonials, theme, design }: any) => {
   return (
     <>
       {theme === "default" && <TestimonialThree testimonials={testimonials} />}
       {theme === "one" && <TestimonialOne testimonials={testimonials} />}
+      {theme === "two" && (
+        <TestimonialTwo testimonials={testimonials} design={design} />
+      )}
     </>
   );
 };
