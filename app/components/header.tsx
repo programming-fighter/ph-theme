c84022edb7516d2dc861d50e0db6365be986df43
ninjastorm24 <siamabdullah1100@gmail.com>
2024-05-27 16:03:10 +0600

    test 1

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -47,50 +47,50 @@ const Header = () => {
   // const { design } = useTheme();
 
   return <p>Hello world</p>;
-  return (
-    <>
-      {design?.header === "default" && <HeaderDefault />}
-      {design?.header === "one" && <HeaderOne />}
-      {design?.header === "two" && <HeaderTwo />}
-      {design?.header === "three" && <HeaderThree />}
-      {design?.header === "four" && <HeaderFour />}
-      {design?.header === "five" && <HeaderFive />}
-      {design?.header === "six" && <HeaderSix />}
-      {design?.header === "seven" && <HeaderSeven />}
-      {design?.header === "eight" && <HeaderEight />}
-      {design?.header === "nine" && <HeaderNine />}
-      {design?.header === "ten" && <HeaderTen />}
-      {design?.header === "eleven" && <HeaderEleven />}
-      {design?.header === "twelve" && <HeaderTwelve />}
-      {design?.header === "thirteen" && <HeaderThirteen />}
-      {design?.header === "fourteen" && <HeaderFourteen />}
-      {design?.header === "fifteen" && <HeaderFifteen />}
-      {design?.header === "sixteen" && <HeaderSixteen />}
-      {design?.header === "seventeen" && <HeaderSeventeen />}
-      {design?.header === "eighteen" && <HeaderEighteen />}
-      {design?.header === "nineteen" && <HeaderNineteen />}
-      {design?.header === "twenty" && <HeaderTwenty />}
-      {design?.header === "twentyone" && <HeaderTwentyOne />}
-      {design?.header === "twentytwo" && <HeaderTwentyTwo />}
-      {design?.header === "twentythree" && <HeaderTwentyThree />}
-      {design?.header === "twentyfour" && <HeaderTwentyFour />}
-      {design?.header === "twentyfive" && <HeaderTwentyFive />}
-      {design?.header === "twentysix" && <HeaderTwentySix />}
-      {design?.header === "twentyseven" && <HeaderTwentySeven />}
-      {design?.header === "twentyeight" && <HeaderTwentyEight />}
-      {design?.header === "twentynine" && <HeaderTwentyNine />}
-      {design?.header === "thirty" && <HeaderThirty />}
-      {design?.header === "thirtyone" && <HeaderThirtyOne />}
-      {design?.header === "thirtythree" && <HeaderThirtyThree />}
-      {design?.header === "thirtyfour" && <HeaderThirtyFour />}
-      {design?.header === "thirtyfive" && <HeaderThirtyFive />}
-      {design?.header === "thirtysix" && <HeaderThirtySix />}
-      {design?.header === "thirtyseven" && <HeaderThirtySeven />}
-      {design?.header === "thirtyeight" && <HeaderThirtyEight />}
-      {design?.header === "thirtynine" && <HeaderThirtyNine />}
-      {design?.header === "forty" && <HeaderForty />}
-    </>
-  );
+  // return (
+  //   <>
+  //     {design?.header === "default" && <HeaderDefault />}
+  //     {design?.header === "one" && <HeaderOne />}
+  //     {design?.header === "two" && <HeaderTwo />}
+  //     {design?.header === "three" && <HeaderThree />}
+  //     {design?.header === "four" && <HeaderFour />}
+  //     {design?.header === "five" && <HeaderFive />}
+  //     {design?.header === "six" && <HeaderSix />}
+  //     {design?.header === "seven" && <HeaderSeven />}
+  //     {design?.header === "eight" && <HeaderEight />}
+  //     {design?.header === "nine" && <HeaderNine />}
+  //     {design?.header === "ten" && <HeaderTen />}
+  //     {design?.header === "eleven" && <HeaderEleven />}
+  //     {design?.header === "twelve" && <HeaderTwelve />}
+  //     {design?.header === "thirteen" && <HeaderThirteen />}
+  //     {design?.header === "fourteen" && <HeaderFourteen />}
+  //     {design?.header === "fifteen" && <HeaderFifteen />}
+  //     {design?.header === "sixteen" && <HeaderSixteen />}
+  //     {design?.header === "seventeen" && <HeaderSeventeen />}
+  //     {design?.header === "eighteen" && <HeaderEighteen />}
+  //     {design?.header === "nineteen" && <HeaderNineteen />}
+  //     {design?.header === "twenty" && <HeaderTwenty />}
+  //     {design?.header === "twentyone" && <HeaderTwentyOne />}
+  //     {design?.header === "twentytwo" && <HeaderTwentyTwo />}
+  //     {design?.header === "twentythree" && <HeaderTwentyThree />}
+  //     {design?.header === "twentyfour" && <HeaderTwentyFour />}
+  //     {design?.header === "twentyfive" && <HeaderTwentyFive />}
+  //     {design?.header === "twentysix" && <HeaderTwentySix />}
+  //     {design?.header === "twentyseven" && <HeaderTwentySeven />}
+  //     {design?.header === "twentyeight" && <HeaderTwentyEight />}
+  //     {design?.header === "twentynine" && <HeaderTwentyNine />}
+  //     {design?.header === "thirty" && <HeaderThirty />}
+  //     {design?.header === "thirtyone" && <HeaderThirtyOne />}
+  //     {design?.header === "thirtythree" && <HeaderThirtyThree />}
+  //     {design?.header === "thirtyfour" && <HeaderThirtyFour />}
+  //     {design?.header === "thirtyfive" && <HeaderThirtyFive />}
+  //     {design?.header === "thirtysix" && <HeaderThirtySix />}
+  //     {design?.header === "thirtyseven" && <HeaderThirtySeven />}
+  //     {design?.header === "thirtyeight" && <HeaderThirtyEight />}
+  //     {design?.header === "thirtynine" && <HeaderThirtyNine />}
+  //     {design?.header === "forty" && <HeaderForty />}
+  //   </>
+  // );
 };
 
 export default Header;
