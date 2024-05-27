1165d15a7faf38e1360a3635811777619011559c
ninjastorm24 <siamabdullah1100@gmail.com>
2024-05-27 16:40:34 +0600

    header test

--- app/hooks/use-data.ts
+++ app/hooks/use-data.ts
@@ -355,7 +355,7 @@ const useData = () => {
     const res = await axios.post(
       "https://admin.ebitans.com/api/v1/" + "getsubdomain/name",
       {
-        name: "siam.localhost:3000",
+        name: "https://ebitans-nextjs-theme.vercel.app/",
       }
     );
 
