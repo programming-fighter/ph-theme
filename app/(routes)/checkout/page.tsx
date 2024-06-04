5500be93ef3209a2a382707ef0dd8ca7c0dc22d2
ninjastorm24 <siamabdullah1100@gmail.com>
2024-06-04 16:17:39 +0600

    checkout page

--- app/(routes)/checkout/page.tsx
+++ app/(routes)/checkout/page.tsx
@@ -3,12 +3,14 @@ import Checkout from "@/app/components/checkout";
 import React from "react";
 import { useSelector } from "react-redux";
 import Login from "../login/page";
+import useTheme from "@/app/hooks/use-theme";
 
 const CheckoutPage = () => {
   const { user } = useSelector((state: any) => state.auth);
+  const { design } = useTheme();
 
   if (user?.verify) {
-    return <Checkout />;
+    return <Checkout theme={design?.checkout_page} />;
   } else {
     return <Login />;
   }
--- app/components/checkout.tsx
+++ app/components/checkout.tsx
@@ -6,10 +6,43 @@ import CheckOutEleven from "./(checkout)/checkout-eleven/checkout-eleven";
 import CheckOutTwentyOne from "./(checkout)/checkout-twentyone/checkout-twentyone";
 import CheckOutForty from "./(checkout)/checkout-fourty/checkout-forty";
 
-const Checkout = () => {
+const Checkout = ({ theme }: any) => {
   return (
     <div>
-      <CheckOutForty />
+      {theme === "one" && <CheckOutFour />}
+      {theme === "two" && <CheckOutFour />}
+      {theme === "three" && <CheckOutSeven />}
+      {theme === "four" && <CheckOutFour />}
+      {theme === "five" && <CheckOutFour />}
+      {theme === "six" && <CheckOutsFive />}
+      {theme === "seven" && <CheckOutSeven />}
+      {theme === "eight" && <CheckOutEleven />}
+      {theme === "nine" && <CheckOutFour />}
+      {theme === "ten" && <CheckOutEleven />}
+      {theme === "eleven" && <CheckOutEleven />}
+      {theme === "twelve" && <CheckOutEleven />}
+      {theme === "thirteen" && <CheckOutFour />}
+      {theme === "fourteen" && <CheckOutsFive />}
+      {theme === "fifteen" && <CheckOutSeven />}
+      {theme === "sixteen" && <CheckOutEleven />}
+      {theme === "seventeen" && <CheckOutFour />}
+      {theme === "eighteen" && <CheckOutsFive />}
+      {theme === "nineteen" && <CheckOutSeven />}
+      {theme === "twenty" && <CheckOutSeven />}
+      {theme === "twentyone" && <CheckOutTwentyOne />}
+      {theme === "twentytwo" && <CheckOutTwentyOne />}
+      {theme === "twentythree" && <CheckOutFour />}
+      {theme === "twentyfour" && <CheckOutTwentyOne />}
+      {theme === "twentyfive" && <CheckOutFour />}
+      {theme === "twentysix" && <CheckOutEleven />}
+      {theme === "twentyseven" && <CheckOutTwentyOne />}
+      {theme === "twentyeight" && <CheckOutTwentyOne />}
+      {theme === "twentynine" && <CheckOutTwentyOne />}
+      {theme === "thirty" && <CheckOutEleven />}
+      {theme === "thirtyone" && <CheckOutTwentyOne />}
+      {theme === "thirtyfive" && <CheckOutTwentyOne />}
+      {theme === "thirtysix" && <CheckOutTwentyOne />}
+      {theme === "forty" && <CheckOutForty />}
     </div>
   );
 };
