e432cb847242ae9312a78e2a7094b0fa6aa7a195
ninjastorm24 <siamabdullah1100@gmail.com>
2024-06-24 11:42:29 +0600

    asdas

--- app/(routes)/about/page.tsx
+++ app/(routes)/about/page.tsx
@@ -1,8 +1,12 @@
 import React from "react";
-const AboutPage = () => {
-  return <div>AboutPage</div>;
+
+const AboutPage: React.FC = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>This is the about page.</p>
+    </div>
+  );
 };
 
 export default AboutPage;
-
-//
