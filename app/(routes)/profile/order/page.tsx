09d56290a2cdb9e10cbd3d0f031d48ce63518c31
ninjastorm24 <siamabdullah1100@gmail.com>
2024-06-25 15:03:25 +0600

    pkg cng

--- app/(routes)/profile/order/page.tsx
+++ app/(routes)/profile/order/page.tsx
@@ -1,8 +1,8 @@
 "use client";
-import OrderComponent from "@/app/components/order-component";
 import React from "react";
 import { useSelector } from "react-redux";
 import Login from "../../login/page";
+import OrderComponent from "@/app/components/order-component";
 
 const OrderPage = () => {
   const { user } = useSelector((state: any) => state.auth);
