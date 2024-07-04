ec52cf2d1f7723047afc36c2dd12251de14a619e
ninjastorm24 <siamabdullah1100@gmail.com>
2024-07-04 10:33:15 +0600

    minor change

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} lg2`}>
+      <body className={`${inter.className} lg2 `}>
         <AppWrapper>{children}</AppWrapper>
       </body>
     </html>
