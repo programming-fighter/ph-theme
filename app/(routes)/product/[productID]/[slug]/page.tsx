e2533110bd0d1ebb02dd50ef419296f16b1a2269
ninjastorm24 <siamabdullah1100@gmail.com>
2024-07-16 10:23:12 +0600

    comment out generatemetadata

--- app/(routes)/product/[productID]/[slug]/page.tsx
+++ app/(routes)/product/[productID]/[slug]/page.tsx
@@ -11,24 +11,24 @@ type Props = {
   params: { productID: string; slug: string };
 };
 
-export async function generateMetadata(
-  { params }: Props,
-  parent: ResolvingMetadata
-): Promise<Metadata> {
-  const url = getUrl();
-  const subDomainData = await fetchDomainData(url);
-  const { design, headersetting } = subDomainData;
-  const product = await getProductDetails({
-    store_id: design?.store_id,
-    product_id: params.productID,
-  });
-  const { name } = product;
-  const websiteName = capitalizeFirstLetter(headersetting.website_name);
-  return {
-    title: `${websiteName} | ${name}`,
-    icons: { icon: imgUrl + headersetting.favicon },
-  };
-}
+// export async function generateMetadata(
+//   { params }: Props,
+//   parent: ResolvingMetadata
+// ): Promise<Metadata> {
+//   const url = getUrl();
+//   const subDomainData = await fetchDomainData(url);
+//   const { design, headersetting } = subDomainData;
+//   const product = await getProductDetails({
+//     store_id: design?.store_id,
+//     product_id: params.productID,
+//   });
+//   const { name } = product;
+//   const websiteName = capitalizeFirstLetter(headersetting.website_name);
+//   return {
+//     title: `${websiteName} | ${name}`,
+//     icons: { icon: imgUrl + headersetting.favicon },
+//   };
+// }
 
 const SingleProductDetails = async ({
   params,
--- app/page.tsx
+++ app/page.tsx
@@ -5,18 +5,18 @@ import { getSubdomainName } from "@/lib";
 import capitalizeFirstLetter from "@/helper/capitalize-first-letter";
 import { imgUrl } from "./site-settings/siteUrl";
 
-export async function generateMetadata() {
-  const url = getUrl();
-  const {
-    headersetting: { website_name, favicon },
-  } = await getSubdomainName(url);
-  const websiteName = capitalizeFirstLetter(website_name);
+// export async function generateMetadata() {
+//   const url = getUrl();
+//   const {
+//     headersetting: { website_name, favicon },
+//   } = await getSubdomainName(url);
+//   const websiteName = capitalizeFirstLetter(website_name);
 
-  return {
-    title: `${websiteName} | Home`,
-    icons: { icon: imgUrl + favicon },
-  };
-}
+//   return {
+//     title: `${websiteName} | Home`,
+//     icons: { icon: imgUrl + favicon },
+//   };
+// }
 
 export default async function Home() {
   const url = getUrl();
