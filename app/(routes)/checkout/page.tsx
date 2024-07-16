31001a3c383e6fb4776b529a81eb7771658ffbe1
ninjastorm24 <siamabdullah1100@gmail.com>
2024-07-16 16:55:47 +0600

    checkout metadata

--- app/(routes)/checkout/page.tsx
+++ app/(routes)/checkout/page.tsx
@@ -5,17 +5,17 @@ import { getSubdomainName } from "@/lib";
 import capitalizeFirstLetter from "@/helper/capitalize-first-letter";
 import { imgUrl } from "@/app/site-settings/siteUrl";
 
-// export async function generateMetadata() {
-//   const url = getUrl();
-//   const subDomainData = await getSubdomainName(url);
-//   const { headersetting } = subDomainData;
-//   const websiteName = capitalizeFirstLetter(headersetting.website_name);
+export async function generateMetadata() {
+  const url = getUrl();
+  const subDomainData = await getSubdomainName(url, "headersetting");
+  const { headersetting } = subDomainData;
+  const websiteName = capitalizeFirstLetter(headersetting.website_name);
 
-//   return {
-//     title: `${websiteName} | Checkout`,
-//     icons: { icon: imgUrl + headersetting.favicon },
-//   };
-// }
+  return {
+    title: `${websiteName} | Checkout`,
+    icons: { icon: imgUrl + headersetting.favicon },
+  };
+}
 
 const CheckoutPage = () => {
   return <CheckoutComponent />;
